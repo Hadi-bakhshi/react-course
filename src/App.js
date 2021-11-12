cac85990a9ef109bbb7b37db1b81cc68fc1ead7a
Hadi <hadibakhshi277@gmail.com>
2021-11-12 22:07:18 +0330

    App updated to Functional Component

--- src/App.js
+++ src/App.js
@@ -1,73 +1,70 @@
-import React, { Component } from "react";
+import { useState } from "react";
 import "./App.css";
 import ProductList from "./Components/ProductList/ProductList";
 import NavBar from "./Components/NavBar/NavBar";
 import Wrapper from "./Components/hoc/Wrapper";
 
-class App extends Component {
-  state = {
-    products: [
-      { title: "React.js", price: "$99", id: 1, quantity: 1 },
-      { title: "Node.js", price: "$89", id: 2, quantity: 2 },
-      { title: "JavaScript", price: "$79", id: 3, quantity: 3 },
-    ],
+const App = () => {
+  
+  const [products,setProducts] = useState([ { title: "React.js", price: "$99", id: 1, quantity: 1 },
+     { title: "Node.js", price: "$89", id: 2, quantity: 2 },
+      { title: "JavaScript", price: "$79", id: 3, quantity: 3 },])
+
+
+    const removeHandler = (id) => {
+    
+    const filteredProducts = products.filter((p) => p.id !== id);
+    setProducts(filteredProducts);
   };
 
-  // Handler
-
-  removeHandler = (id) => {
-    // console.log("clicked", id );
-    const filteredProducts = this.state.products.filter((p) => p.id !== id);
-    this.setState({ products: filteredProducts });
-  };
-
-  incrementHandler = (id) => {
-    const index = this.state.products.findIndex((item) => item.id === id);
-    const product = { ...this.state.products[index] };
+    const  incrementHandler = (id) => {
+    const index =products.findIndex((item) => item.id === id);
+    const product = { ...products[index] };
     product.quantity++;
-    const products = [...this.state.products];
-    products[index] = product;
-    this.setState({ products });
+    const updatedProducts = [...products];
+    updatedProducts[index] = product;
+    setProducts(updatedProducts);
   };
 
-  decrementHandler = (id) => {
-    const index = this.state.products.findIndex((item) => item.id === id);
-    const product = { ...this.state.products[index] };
+  
+  const decrementHandler = (id) => {
+    const index = products.findIndex((item) => item.id === id);
+    const product = { ...products[index] };
     if (product.quantity === 1) {
-      const filteredProducts = this.state.products.filter((p) => p.id !== id);
-      this.setState({ products: filteredProducts });
+      const filteredProducts = products.filter((p) => p.id !== id);
+      setProducts(filteredProducts);
     } else {
-      const products = [...this.state.products];
+      const updatedProducts = [...products];
       product.quantity--;
-      products[index] = product;
-      this.setState({ products });
+      updatedProducts[index] = product;
+      setProducts(updatedProducts);
     }
   };
 
-  changeHandler = (event, id) => {
-    const index = this.state.products.findIndex((item) => item.id === id);
-    const product = { ...this.state.products[index] };
-    product.title = event.target.value;
-    const products = [...this.state.products];
-    products[index] = product;
-    this.setState({ products });
-  };
+  const changeHandler = (event, id) => {
+        const index = products.findIndex((item) => item.id === id);
+        const product = { ...products[index] };
+        product.title = event.target.value;
+        const updatedProducts = [...products];
+        updatedProducts[index] = product;
+        setProducts(updatedProducts);
+      };
 
-  render() {
-    return (
-      <>
-        <NavBar
-          totalItems={this.state.products.filter((p) => p.quantity > 0).length}
-        />
-        <ProductList
-          products={this.state.products}
-          onDelete={this.removeHandler}
-          onInc={this.incrementHandler}
-          onDec={this.decrementHandler}
-          onChange={this.changeHandler}
-        />
-      </>
-    );
-  }
+
+  return ( 
+    <>
+         <NavBar
+           totalItems={products.filter((p) => p.quantity > 0).length}
+         />
+         <ProductList
+           products={products}
+           onDelete={removeHandler}
+           onInc={incrementHandler}
+           onDec={decrementHandler}
+           onChange={changeHandler}
+         />
+       </>
+   );
 }
+ 
 export default Wrapper(App, "container");
